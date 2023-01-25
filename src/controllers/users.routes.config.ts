97c07368bab669dbdb25412e1bb9ae5f8250821f
Kseniia Tokareva <Kseniia_Tokareva@epam.com>
2023-01-25 16:53:26 +0300

    fix password validation

--- src/controllers/users.routes.config.ts
+++ src/controllers/users.routes.config.ts
@@ -5,16 +5,17 @@ import { CommonRoutesConfig } from "../config/common.routes.config";
 import usersController from "./users.controller";
 
 const validator = createValidator();
+const passwordRegEx = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/g;
 
 const createUserSchema = Joi.object({
   login: Joi.string().required(),
-  password: Joi.string().alphanum().required(),
+  password: Joi.string().pattern(passwordRegEx).required(),
   age: Joi.number().integer().min(4).max(130).required(),
 });
 
 const updateUserSchema = Joi.object({
   login: Joi.string(),
-  password: Joi.string().alphanum(),
+  password: Joi.string().pattern(passwordRegEx),
   age: Joi.number().integer().min(4).max(130),
 });
 
